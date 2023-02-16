0edc52801e49d740301631e05916ab4de9cf401d
Aleksey <lexandreeev@gmail.com>
2023-02-16 20:28:49 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿var array = new[] { 1,9,9,0,2,8,0,9 };
+var group = array.GroupBy(i => i);
+int count = 0;
+foreach (var collection in group)
+{
+  if (collection.Count() == 1)
+  {
+    count++;
+    Console.WriteLine($"Элемент {collection.Key} повторяется {collection.Count()} раз");
+  }
+  else
+  Console.WriteLine($"Элемент {collection.Key} повторяется {collection.Count()} раз");
+}
+Console.ReadKey();
